ce7b71efa71b7778f2ef4316401a0d592ced7f16
SasukeXDev <srk100m@gmail.com>
2026-05-13 00:29:38 +0530

    Update antinsfw.py
    drg

--- plugins/antinsfw.py
+++ plugins/antinsfw.py
@@ -51,6 +51,6 @@ async def check_anti_nsfw(new_name, message):
     for category, keywords in nsfw_keywords.items():
         for keyword in keywords:
             if keyword.lower() in lower_name:
-                await message.reply_text("You can't rename files with NSFW content.")
+                await message.reply_text("You can't rename files with NSFW content. Contact : @Tojma")
                 return True
     return False
